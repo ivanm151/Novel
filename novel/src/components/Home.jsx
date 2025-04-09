962a95f71f3548efe248da1c85797e05cef84068
ivanm151 <ivmedv5@gmail.com>
2025-04-09 17:31:41 +0700

    Home created

---
+++ novel/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <h2>Добро пожаловать в визуальную новеллу!</h2>
+            <p>Это стартовая страница. Здесь будет выбор главы или статьи.</p>
+        </div>
+    );
+}
+
+export default Home;
